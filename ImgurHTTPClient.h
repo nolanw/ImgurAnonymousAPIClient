6f29b7ffcda225f3981a66a2a292df0219fd5160
Nolan Waite <nolan@nolanw.ca>
2014-05-18 08:43:16 -0300

    Add empty ImgurHTTPClient class.

---
+++ ImgurHTTPClient.h
@@ -0,0 +1,10 @@
+//  ImgurHTTPClient.h
+//  ImgurHTTPClient
+//
+//  Public domain. https://github.com/nolanw/ImgurHTTPClient
+
+#import <Foundation/Foundation.h>
+
+@interface ImgurHTTPClient : NSObject
+
+@end
---
+++ ImgurHTTPClient.m
@@ -0,0 +1,10 @@
+//  ImgurHTTPClient.m
+//  ImgurHTTPClient
+//
+//  Public domain. https://github.com/nolanw/ImgurHTTPClient
+
+#import "ImgurHTTPClient.h"
+
+@implementation ImgurHTTPClient
+
+@end
